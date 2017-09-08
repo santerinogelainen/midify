15540ad9b4db3d66d8777df465c0768b9e8e45b8
Santeri <santeri.nogelainen@gmail.com>
2017-09-08 17:05:10 +0300

    first commit

---
+++ src/Midi.cs
@@ -0,0 +1,100 @@
+﻿#define DEBUG
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Midis {
+
+    public class Midi {
+
+        private FileStream stream;
+        public HeaderChunk header = new HeaderChunk();
+
+        public Midi(string file) {
+            this.ReadFile(file);
+        }
+
+        /// <summary>
+        /// initalizes the FileStream variable, and sets all the headers / tracks of the file
+        /// </summary>
+        /// <param name="file"></param>
+        private void ReadFile(string file) {
+            this.stream = new FileStream(file, FileMode.Open);
+            this.ReadHeader();
+        }
+
+        /// <summary>
+        /// reads data into a class made of byte arrays (similar to how structs and fread works in c)
+        /// </summary>
+        /// <param name="to">the object/class where to append</param>
+        /// <param name="offset">offset where to start reading</param>
+        private void ReadStream(object to, int offset = 0) {
+            foreach (FieldInfo field in to.GetType().GetFields()) {
+                byte[] temp = new byte[((Array)field.GetValue(to)).Length];
+                this.stream.Read(temp, offset, ((Array)field.GetValue(to)).Length);
+                to.GetType().GetField(field.Name).SetValue(to, temp);
+            }
+        }
+
+        /// <summary>
+        /// pretty print a class that only has variables made of byte arrays (byte[])
+        /// </summary>
+        /// <param name="o">object to print</param>
+        private void DebugByteObject(object o) {
+            Console.WriteLine("\n{0}", o.GetType().Name);
+            foreach (FieldInfo field in o.GetType().GetFields()) {
+                Console.Write("\n{0, -15}", field.Name);
+                byte[] value = (byte[])field.GetValue(o);
+                Console.Write("{0, -20} ", this.ByteArrayToInt(value));
+                Console.Write("{0, -20} ", BitConverter.ToString(value));
+                foreach (byte b in value) {
+                    Console.Write("{0}", (char)b);
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// convert a byte array of unknown size into a Int64
+        /// </summary>
+        /// <param name="arr">byte array</param>
+        /// <returns>Int64 value of the byte(s)</returns>
+        private Int64 ByteArrayToInt(byte[] arr) {
+            switch (arr.Length) {
+                case sizeof(sbyte):
+                    return (Int64)arr[0];
+                case sizeof(Int16):
+                    return (Int64)BitConverter.ToInt16(arr, 0);
+                case sizeof(Int32):
+                    return (Int64)BitConverter.ToInt32(arr, 0);
+                case sizeof(Int64):
+                    return (Int64)BitConverter.ToInt64(arr, 0);
+            }
+            Console.WriteLine("Too big to convert into an integer.");
+            return -1;
+        }
+
+        /// <summary>
+        /// read the header of the file to this.header
+        /// </summary>
+        private void ReadHeader() {
+            this.ReadStream(this.header);
+#if (DEBUG)
+            this.DebugByteObject(this.header);
+#endif
+        }
+    }
+
+    /// <summary>
+    /// header chunk "structure"
+    /// </summary>
+    public class HeaderChunk {
+        public byte[] prefix = new byte[4];
+        public byte[] length = new byte[4];
+        public byte[] format = new byte[2];
+        public byte[] tracks = new byte[2];
+        public byte[] timing = new byte[2];
+    }
+
+}
---
+++ src/Midify.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Midis;
+
+namespace ConsoleApplication3 {
+    class midify {
+
+        public static readonly string[] options = {"help", "tc", "make"};
+
+        static void Main(string[] args) {
+
+            if (!CheckArgs(args)) {
+                return;
+            }
+
+            string option = args[0];
+
+            switch (option) {
+                case "help":
+                    break;
+
+
+                case "tc":
+                    break;
+
+
+                case "make":
+                    if (args.Length > 1) {
+                        Make(args[1]);
+                    } else {
+                        Console.WriteLine("Syntax: midify make <midifilepath>");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Unknown command '{0}'. The command you requested has been added, but has not been implemented yet.", option);
+                    return;
+            }
+        }
+
+        static void Make(string filepath) {
+            Midi m = new Midi(filepath);
+        }
+
+        /// <summary>
+        /// check the validity of the command line arguments
+        /// </summary>
+        /// <param name="args">command line arguments</param>
+        /// <returns>true if valid</returns>
+        static bool CheckArgs(string[] args) {
+            if (args == null || args.Length < 1) {
+                Console.WriteLine("Syntax: midify <option>");
+                return false;
+            }
+            string option = args[0];
+            return CheckOption(option);
+        }
+
+        /// <summary>
+        /// check the validity/existance of the option given
+        /// </summary>
+        /// <param name="option">option given</param>
+        /// <returns>true if valid and command exists</returns>
+        static bool CheckOption(string option) {
+            foreach (string o in options) {
+                if (o == option) {
+                    return true;
+                }
+            }
+            Console.WriteLine("Unknown command '{0}'. The command you requested does not exist.", option);
+            return false;
+        }
+    }
+}
